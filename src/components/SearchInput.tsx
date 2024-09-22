0638f2e6c7104b4ec3d0328b1a6057603d3e78fb
Marco Hernandez <marcohern@gmail.com>
2024-09-22 10:51:38 -0500

    added search button

--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -1,4 +1,13 @@
-import { Input, InputGroup, InputLeftElement } from "@chakra-ui/react";
+import {
+  Button,
+  HStack,
+  Input,
+  InputGroup,
+  InputLeftElement,
+  Show,
+  Text,
+} from "@chakra-ui/react";
+import { useRef } from "react";
 import { BsSearch } from "react-icons/bs";
 
 interface Props {
@@ -6,19 +15,31 @@ interface Props {
 }
 
 const SearchInput = ({ onSearchByTitle }: Props) => {
+  const ref = useRef<HTMLInputElement>(null);
+
   return (
-    <InputGroup>
-      <InputLeftElement children={<BsSearch />} />
-      <Input
-        borderRadius={20}
-        _placeholder="Search games..."
-        onKeyDown={(event) => {
-          let input = event.target as HTMLInputElement;
-          if (event.code === "Enter") onSearchByTitle(input.value);
-        }}
-      />
-      ;
-    </InputGroup>
+    <form
+      onSubmit={(e) => {
+        e.preventDefault();
+        if (ref.current) onSearchByTitle(ref.current.value);
+      }}
+    >
+      <HStack>
+        <InputGroup>
+          <InputLeftElement children={<BsSearch />} />
+          <Input ref={ref} borderRadius={20} _placeholder="Search games..." />;
+        </InputGroup>
+        <Show above="lg">
+          <InputGroup width={100}>
+            <Button type="submit" borderRadius={10}>
+              <BsSearch />
+
+              <Text paddingLeft={2}>Search</Text>
+            </Button>
+          </InputGroup>
+        </Show>
+      </HStack>
+    </form>
   );
 };
 
