c45f7d4259023af8324b069e76211cd6b667f737
Marco Hernandez <marcohern@gmail.com>
2024-09-24 20:32:43 -0500

    Padding in error page

--- src/pages/ErrorPage.tsx
+++ src/pages/ErrorPage.tsx
@@ -1,5 +1,6 @@
-import { Heading, Text } from "@chakra-ui/react";
+import { Box, Heading, Text } from "@chakra-ui/react";
 import { isRouteErrorResponse, useRouteError } from "react-router-dom";
+import NavBar from "../components/NavBar";
 
 const ErrorPage = () => {
   const error = useRouteError();
@@ -7,10 +8,15 @@ const ErrorPage = () => {
   console.error("ErrorPage", error);
   return (
     <>
-      <Heading>Yikes!</Heading>
-      <Text>
-        {isRouteErrorResponse(error) ? "Invalid page" : "Something went wrong"}
-      </Text>
+      <NavBar />
+      <Box padding={5}>
+        <Heading>Yikes!</Heading>
+        <Text>
+          {isRouteErrorResponse(error)
+            ? "This page does not exist"
+            : "An unexpected error ocurred"}
+        </Text>
+      </Box>
     </>
   );
 };
