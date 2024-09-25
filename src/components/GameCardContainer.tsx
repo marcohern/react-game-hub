d8a2440c17036ac2c9c6b69e849a30375489c00b
Marco Hernandez <marcohern@gmail.com>
2024-09-24 21:05:14 -0500

    added GameCardContainer animation on hover

--- src/components/GameCardContainer.tsx
+++ src/components/GameCardContainer.tsx
@@ -1,13 +1,18 @@
 import { Box } from "@chakra-ui/react";
 import { ReactNode } from "react";
-
+import { motion } from "framer-motion";
 interface Props {
   children: ReactNode;
 }
 
 const GameCardContainer = ({ children }: Props) => {
   return (
-    <Box borderRadius={10} overflow="hidden">
+    <Box
+      as={motion.div}
+      borderRadius={10}
+      overflow="hidden"
+      whileHover={{ scale: 1.05 }}
+    >
       {children}
     </Box>
   );
